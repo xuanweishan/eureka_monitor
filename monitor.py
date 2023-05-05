4aebdfb3b4794e0b195b6b6edf3b4a8f7c053caa
xuanweishan <xuanweishan@gmail.com>
2023-05-06 02:26:53 +0800

    Complete node state functiion, and create get data functions.

--- monitor.py
+++ monitor.py
@@ -114,7 +114,7 @@ Can use only digit and full name.""",
 def run_cli(cmd, results):
     try:
         result = subprocess.check_output(cmd)
-        results[cmd[0]] = result.decode('utf-8').strip()
+        results[os.path.basename(cmd[0])] = result.decode('utf-8').strip()
     except subprocess.CalledProcessError as err:
         print("Error: error occurred while running %s." %(cmd))
         print("    Error code: ", err.returncode)
@@ -131,8 +131,8 @@ def get_node_state():
     alive_nodes : list of alive nodes
     """
     # 1. Command lines to get the node stat data
-    qstat = ['qstat', '-n']
-    pbsnodes = ['pbsnodes', '-a']
+    qstat = ['/usr/local/torque/bin/qstat', '-n']
+    pbsnodes = ['/usr/local/torque/bin/pbsnodes', '-a']
     cmds = [qstat, pbsnodes]
     
     # 2. Run cmds in multiple threads
@@ -149,11 +149,13 @@ def get_node_state():
     # 3. Deal with command results
     # 3.1 qstat
     job_stats = qstat_data_handler(results['qstat'])
-    print(job_stats)
 
     # 3.2 pbsnodes
     nodes_state = pbsnodes_data_handler(results['pbsnodes'])
-    print(nodes_state)
+
+    # 4. Merge messages of pbsnodes and qstat
+    #    Using the structure of pbsnodes as skeleton
+    return merge_pbs_qstat(nodes_state, job_stats)
 
     
 def qstat_data_handler(qstat_msg):
@@ -161,9 +163,11 @@ def qstat_data_handler(qstat_msg):
     Deal with message from command 'qstat -n'
     
     input:
+
     qstat_msg : result string from the command
 
-    output:
+    Return:
+
     job_state : A list of directiionaries to record job stat 
     [{'Job_ID': ***, 'Name': ***, 'User', 'Time': **:**:**, 'Nodes': []}, ]
     """
@@ -173,7 +177,7 @@ def qstat_data_handler(qstat_msg):
     # 2. Deal with message per line
     # 2.1 Initialization of variables required
     nodes = []
-    jobs_state = []
+    jobs_state = {}
 
     # 2.2 Analyze message
     for line in lines:
@@ -183,7 +187,7 @@ def qstat_data_handler(qstat_msg):
         if len(parts) == 11:
             # 2.2.1.1 Add using nodes to previous job state
             if len(jobs_state) > 0:
-                jobs_state[-1]['nodes'] = nodes
+                jobs_state[job_ID]['Nodes'] = nodes
 
             # 2.2.1.2 Create a new job state
             job_ID = parts[0].split('.')[0]
@@ -192,13 +196,12 @@ def qstat_data_handler(qstat_msg):
             stat = parts[9]
             time = parts[10]
             nodes = []
-            jobs_state.append({
-                               'ID': job_ID,
-                               'User': user,
-                               'Name': name,
-                               'Time': time,
-                               'Stat': stat,
-                             })
+            jobs_state[job_ID] = {
+                                  'User': user,
+                                  'Name': name,
+                                  'Time': time,
+                                  'Stat': stat,
+                                 }
         
         # 2.2.2 Grep Using nodes in the job
         elif len(parts) == 1:
@@ -214,34 +217,144 @@ def qstat_data_handler(qstat_msg):
             exit(1)
     
     if len(jobs_state) > 0:
-        jobs_state[-1]['nodes'] = nodes
+        jobs_state[job_ID]['Nodes'] = nodes
 
     return jobs_state
 
 def pbsnodes_data_handler(pbs_msg):
+    """
+    Deal with the output message of command "pbsnodes".
+
+    input:
+        pbs_msg : a srting from command "pbsnodes".
+    
+    output:
+        nodes : a dictionary with keys of node names, 
+                for each node is also a dictionary store keys of 'state', 'jobs'
+                'jobs' is also a dictionary with keys of job IDs, store the threads on that node.
+        nodes = {
+                'eurekaXX': {'state': 'xx', 'jobs': {'ID1': xxxx, 'ID2': xxxx}},
+                }
+    """
+    
+    # 1. Initialize the data will be return
     nodes = {}
 
+    # 2. Grep needed state data line by line
     for line in pbs_msg.splitlines():
+        
+        # 2.1 Get node name and initial node state dict
         if line.startswith('eureka'):
             node_name = line
-            nodes[node_name] = {'state': 'unknown', 'jobs': []}
+            nodes[node_name] = {'State': 'unknown', 'Jobs': {}}
+        
+        # 2.2 Get node state
         elif ' state =' in line:
-            nodes[node_name]['state'] = line.split()[2]
+            nodes[node_name]['State'] = line.split()[2]
+        
+        # 2.3 Get jobs on the node
         elif ' jobs =' in line:
             jobs = line.split()[2:]
             jobs_info = {}
             for job in jobs:
                 job_ID = job.split('.')[0].split('/')[1]
                 if job_ID in jobs_info:
-                    jobs_info[job_ID] += 1
+                    jobs_info[job_ID]['np'] += 1
                 else:
-                    jobs_info[job_ID] = 1
-            nodes[node_name]['jobs'].append(jobs_info)
+                    jobs_info[job_ID] = {'np': 1}
+            nodes[node_name]['Jobs'] = jobs_info
         else:
             continue
 
     return nodes
 
+def merge_pbs_qstat(pbs, qstat):
+    """
+    Merge the messages from commane "pbsnodes" and "qstat"
+
+    Input:
+
+        pbs: A dictionary recording the node state from "pbsnodes"
+             Will be used as the skeleton of return data
+        
+        qstat: A list recording the jobs info.
+
+    Return:
+
+        nodes_state: 
+            A dictionary recording the stats and jobs info on each node.
+
+    """
+    # 1. Arrange messages node by node.
+
+    for node in pbs:
+        for job in pbs[node]['Jobs']:
+            if job not in qstat:
+                continue
+            pbs[node]['Jobs'][job]['Name'] = qstat[job]['Name']
+            pbs[node]['Jobs'][job]['User'] = qstat[job]['User']
+            pbs[node]['Jobs'][job]['Time'] = qstat[job]['Time']
+    
+    return pbs
+
+def get_alive_nodes(node_state):
+    alive_nodes = []
+    for node in node_state:
+        if node_state[node]['State'] in ['job-exclusive', 'free']:
+            alive_nodes.append(node)
+
+    return alive_nodes
+
+def get_cpu_info(alive_node):
+    """
+    Get CPU usage, temprature
+
+    Input:
+        alive_node : A list record alive nodes
+
+    Return:
+        cpu_usage : A dictionary with key of node names, save a dictionary with info of 'Usage' and 'Temp'
+    """
+
+    cmds = {
+            'usage' : ['/usr/bin/mpstat'],
+            'temp' : ['/usr/bin/sensors'],
+           }
+    return 0
+
+def get_memory_usage(alive_node):
+    """
+    """
+    
+    cmd = ['/usr/bin/free']
+    return 0
+
+def get_gpu_usage(alive_node):
+    """
+    """
+
+    cmd = ['/usr/bin/nvidia-smi', '-q']
+    return 0
+
+def get_IB_speed(alive_node):
+    """
+    """
+
+    cmd = ['/usr/sbin/iblinkinfo', '-l']
+    return 0
+
+def get_disk_usage(alive_node):
+    """
+    """
+
+    cmd = ['/usr/bin/df']
+    return 0
+
+def merge_data(state, cpu, mem, gpu, ib, disk):
+    return 0
+
+def output(data):
+    return 0
 
 if __name__ == "__main__":
     # 1. Handle arguments
@@ -249,14 +362,30 @@ if __name__ == "__main__":
 
     # 2. Get node state : alive_or_dead user job_name job_ID Time
     node_state = get_node_state()
+    print(node_state)
+    alive_nodes = get_alive_nodes(node_state)
+    print(alive_nodes)
 
     # 3. Get node datas
-    # 3.2 CPU : Usage and Temp.
-    # 3.3 Memory usage
-    # 3.4 GPU : Usage Mem_Usage Temp.
-    # 3.5 IB : Status Speed
-    # 3.6 Disk : Usage
+    
+    # 3.1 CPU : Usage and Temp.
+    cpu_usage = get_cpu_info(alive_nodes)
+    
+    # 3.2 Memory usage
+    mem_usage = get_memory_usage(alive_nodes)
+
+    # 3.3 GPU : Usage Mem_Usage Temp.
+    gpu_usage = get_gpu_usage(alive_nodes)
+
+    # 3.4 IB : Status Speed
+    IB_speed = get_IB_speed(alive_nodes)
+
+    # 3.5 Disk : Usage
+    disk_usage = get_disk_usage(alive_nodes)
 
     # 4. Merge data as a table
+    output_data = merge_data(node_state, cpu_usage, mem_usage, gpu_usage, IB_speed, disk_usage)
 
     # 5. Output
+    output(output_data)
+    
